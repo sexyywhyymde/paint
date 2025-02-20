60877cb7befbed34488d46dc89482fa44a03fb8b
Andrey <setikonelove@gmail.com>
2025-02-20 15:06:52 +0300

    add eraser and color

--- javascript.js
+++ javascript.js
@@ -2,15 +2,18 @@ const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 const brush = document.getElementById("Brush");
 const clear = document.getElementById("clear");
-const size = document.getElementById("size")
+const size = document.getElementById("size");
+const color = document.getElementById("color");
+const eraser = document.querySelector("#eraser");
 
 let isDrawing = false;
 let lastX = 0;
 let lastY = 0;
+let isEraser = false;
 
 canvas.addEventListener("mousedown", (e) => {
   lastX = e.offsetX;
-  lastX = e.offsetY;
+  lastY = e.offsetY;
   isDrawing = true;
 });
 
@@ -18,6 +21,13 @@ canvas.addEventListener("mousemove", (e) => {
   if (isDrawing) {
     const x = e.offsetX;
     const y = e.offsetY;
+
+    
+    if (isEraser) {
+      ctx.strokeStyle = "white";
+    } else {
+      ctx.strokeStyle = color.value;
+    }
 
     ctx.lineWidth = size.value;
     ctx.lineCap = "round";
@@ -34,9 +44,13 @@ canvas.addEventListener("mousemove", (e) => {
 
 canvas.addEventListener("mouseup", () => {
   isDrawing = false;
+  isEraser = false
 });
 
 clear.addEventListener("click", () => {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
 });
 
+eraser.addEventListener("click", ()=>{
+  isEraser=true
+} )
