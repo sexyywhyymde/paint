ad177ef2ee37be0e41f57d7f708bd0a4d184e02d
Andrey <setikonelove@gmail.com>
2025-02-27 14:24:42 +0300

    add button fill

--- javascript.js
+++ javascript.js
@@ -1,15 +1,21 @@
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
-const brush = document.getElementById("Brush");
+const brush = document.getElementById("brush");
 const clear = document.getElementById("clear");
 const size = document.getElementById("size");
 const color = document.getElementById("color");
 const eraser = document.querySelector("#eraser");
+const fill = document.getElementById("fill");
+console.log(brush);
+
+canvas.width = window.innerWidth * 0.8;
+canvas.height = window.innerHeight * 0.8;
 
 let isDrawing = false;
 let lastX = 0;
 let lastY = 0;
 let isEraser = false;
+let isFill = false;
 
 canvas.addEventListener("mousedown", (e) => {
   lastX = e.offsetX;
@@ -22,7 +28,6 @@ canvas.addEventListener("mousemove", (e) => {
     const x = e.offsetX;
     const y = e.offsetY;
 
-    
     if (isEraser) {
       ctx.strokeStyle = "white";
     } else {
@@ -42,15 +47,44 @@ canvas.addEventListener("mousemove", (e) => {
   }
 });
 
+canvas.addEventListener("click", (e) => {
+  if (isFill) {
+    ctx.fillStyle = color.value;
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+  }
+});
+
 canvas.addEventListener("mouseup", () => {
   isDrawing = false;
-  isEraser = false
+  isEraser = false;
+});
+
+fill.addEventListener("click", () => {
+  isFill = !isFill;
+
+  fill.classList.add("active");
+  brush.classList.remove("active")
+  eraser.classList.remove("active")
 });
 
 clear.addEventListener("click", () => {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
 });
 
-eraser.addEventListener("click", ()=>{
-  isEraser=true
-} )
+eraser.addEventListener("click", () => {
+  isEraser = true;
+  isFill = false;
+
+  eraser.classList.add("active");
+  fill.classList.remove("active")
+  brush.classList.remove("active")
+});
+
+brush.addEventListener("click", () => {
+  isEraser = false;
+  isFill = false;
+
+  brush.classList.add("active");
+  fill.classList.remove("active")
+  eraser.classList.remove("active")
+});
